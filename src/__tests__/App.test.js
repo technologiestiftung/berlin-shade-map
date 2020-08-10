b0a01c68a2f2847b80830568b47d9158b1923172
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-10 20:30:05 +0200

    add jest-fetch-mock module

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,10 @@
 import { render } from "../test-utils";
+import fetchMock from "jest-fetch-mock";
 
 const { findByText } = render();
+beforeEach(() => {
+  fetchMock.doMock();
+});
 
 describe("App", () => {
   test("sidebar opens and displays title on initial load", async () => {

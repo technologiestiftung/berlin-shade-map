1f7b83faa2aa75c7a7af246ce4296facecf048bf
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-11-10 17:13:28 +0100

    chore(SHADOW-25): indicate prototype state of map/shades

--- src/config.js
+++ src/config.js
@@ -47,6 +47,11 @@ export default {
       {
         title: "",
         content:
+          "<i>Bei der Karte handelt es sich um einen Prototyp. Es kann zu gelegentlichen Unregelmäßigkeiten in der Darstellung der Schatten kommen.</i>",
+      },
+      {
+        title: "",
+        content:
           "<a href='https://www.technologiestiftung-berlin.de/de/impressum/' target='_blank' rel='noopener noreferrer'>Impressum</a> | <a href='https://www.technologiestiftung-berlin.de/de/datenschutz/' target='_blank' rel='noopener noreferrer'>Datenschutz</a>",
       },
     ],
